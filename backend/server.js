c1432c635877d2a3d5d956a9a35c8f4b20f4ef0d
Y1nch <bede0221@gmail.com>
2026-05-26 11:43:43 +0800

    V1.14
    mysql2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
